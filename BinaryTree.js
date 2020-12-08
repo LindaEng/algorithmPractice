24a4f62bf8f977c1d4d2a24b0273598ecad2dc20
Linda Eng <90lineng@gmail.com>
2020-12-08 14:18:27 -0500

    Binary Tree: contains method

--- BinaryTree.js
+++ BinaryTree.js
@@ -7,6 +7,7 @@
 //Binary Search Tree implementation
 //1) Implement the Node class to create a BST. the constructor should initialize values 'data', 'left', and 'right'
 //2) Implement the 'insert' method for the Node class. Insert should accept an argument 'data', then create an insert a new node at the appropriate location in the tree.
+//3) Implement the 'contains' method for the Node class. Contains should accept a 'data' argument and return the Node in the tree with the same value. If the value isnt in the tree return null.
 
 class Node {
   constructor(data) {
@@ -18,7 +19,7 @@ class Node {
   insert(data) {
     //keep going left or right until its null
     if(data < this.data && this.left) {
-      //recursion 
+      //recursion
        this.left.insert(data)
     } else if (data < this.data) {
       this.left = new Node(data)
@@ -27,5 +28,21 @@ class Node {
     } else if (data > this.data) {
       this.right = new Node(data)
     }
+  }
+
+  contains(data) {
+    //base case
+    if(this.data === data) {
+      return this
+    }
+
+    //recurse right if data is greater left if less
+    if(this.data < data && this.right) {
+      this.right.contains(data)
+    } else if (this.data > data && this.left) {
+      this.left.contains(data)
+    }
+
+    return null
   }
 }
