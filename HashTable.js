7b836bb18041e7ca29c5efb7edb71a6b917cffea
Linda Eng <90lineng@gmail.com>
2021-03-23 14:47:37 -0400

    Hashtable complete

--- HashTable.js
+++ HashTable.js
@@ -1,30 +1,49 @@
-class HashTable {
-  constructor(size) {
-    this.data = new Array(size)
+function hashStringToInt(s, tableSize) {
+  let hash = 7
+  for(let i = 0; i < s.length; i++) {
+    hash = (17 * s.charCodeAt(i) * tableSize) % tableSize
   }
-
-  _hash(key) {
-    let hash = 0
-    for(let i = 0; i < key.length; i++) {
-      hash = ( hash + key.charCodeAt(i) * i) % this.data.length
-    }
-    return hash
-  }
-
-  set(input, value) {
-    let key = this._hash(input)
-    this.data[key] = value
-  }
-
-  get(input) {
-    let key = this._hash(input)
-    return this.data[key]
-  }
-
+  return hash
 }
 
-const myHashTable = new HashTable(50);
-myHashTable.set('grapes', 10000)
-myHashTable.get('grapes')
-myHashTable.set('apples', 9)
-myHashTable.get('apples')
+class HashTable {
+  constructor(size) {
+    this.table = new Array(size)
+    this.numItems = 0
+    this.loadFactor = this.numItems / this.table.length
+  }
+
+  resize = () => {
+    const newTable = new Array(this.table.length * 2)
+    this.table.forEach(item => {
+      item.forEach(([key, value]) => {
+        const idx = hashStringToInt(key, newTable.length)
+        if(newTable[idx]){
+          newTable[idx].push([key, value])
+        } else {
+          newTable[idx] = [[key,value]]
+        }
+      })
+    })
+
+  }
+
+  setItem = (key, value) => {
+    this.numItems++
+    if(this.loadFactor > .8) this.resize()
+
+    const idx = hashStringToInt(key, tableSize)
+    if(this.table[idx]) {
+      this.table[idx].push([key,value])
+    }else {
+      this.table[idx] = [[key,value]]
+    }
+  }
+
+  getItem = (key) => {
+    const idx = hashStringToInt(key)
+    if(this.table[idx]) {
+      return this.table[idx].find(x => x[0] === key)[1]
+    }else return null
+  }
+}
